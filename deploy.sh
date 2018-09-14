566caee8a35580bd31d18a1852767843857f9682
Mat Allen <mallen@redhat.com>
2018-09-14 10:44:20 -0500

    auto deployment config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 
+NAMESPACE=data-backup
+
 oc new-project data-backup
 oc process --param=APPLICATION_NAME=data-backup -f backup-build.yaml | oc apply -f-
 oc process --param=NAMESPACE=$NAMESPACE --param=APPLICATION_NAME=data-backup -f backup-deployment.yaml | oc apply -f-
