2ed779576f9a1141eb9e90c738966f1130e668ca
Mat Allen <mallen@redhat.com>
2020-01-16 09:21:56 -0600

    added capability for multiple slack channel alerts

--- src/main/java/com/redhat/sso/backup/Alert.java
+++ src/main/java/com/redhat/sso/backup/Alert.java
@@ -6,8 +6,14 @@ public class Alert{
 	public void alert(String taskName, String taskUrl, int responseCode){
 		if ("true".equalsIgnoreCase(Config.get().getOptions().get("slack.webhook.notifications"))){
 			String slackTemplate=Config.get().getOptions().get("slack.webhook.template");
-			String slackUrl=Config.get().getOptions().get("slack.webhook.url");
-			Http.post(slackUrl, slackTemplate.replaceAll("SERVER_NAME", taskName).replaceAll("SERVER_URL", taskUrl));
+//			String slackUrl=Config.get().getOptions().get("slack.webhook.url");
+//			Http.post(slackUrl, slackTemplate.replaceAll("SERVER_NAME", taskName).replaceAll("SERVER_URL", taskUrl));
+			
+			String slackUrls=Config.get().getOptions().get("slack.webhook.urls");
+			for (String slackUrl:slackUrls.split(",")){
+				Http.post(slackUrl, slackTemplate.replaceAll("SERVER_NAME", taskName).replaceAll("SERVER_URL", taskUrl));	
+			}
+			
 		}
 	}
 }
