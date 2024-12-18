a60434ee88e00102668b8f3a546c626a34b61742
Mat Allen <mallen@redhat.com>
2024-12-18 09:06:27 -0600

    update

--- src/main/java/com/redhat/sso/backup/AlertChat.java
+++ src/main/java/com/redhat/sso/backup/AlertChat.java
@@ -24,30 +24,31 @@ public class AlertChat{
 		if ("true".equalsIgnoreCase(Config.get().getOptions().get("notifications.enabled"))){
 //		  System.out.println("send alert - notification on");
 			for(Map<String, String> notification:c.getNotifications()){
+				if ("true".equalsIgnoreCase(notification.get("enabled"))){
 					List<String> events=Arrays.asList(notification.get("events").split(","));
 					if (events.contains(type.name())){
 						// send the notification!
 						String channel=notification.get("channel");
 						String template=getTemplate(c, channel);
 						String alertPayload=template.replaceAll("TASK_NAME", name).replaceAll("RESPONSE_CODE", httpResponse).replaceAll("TASK_URL", taskUrl);
-						System.out.println(String.format("sending this; channel=%s, payload=%s", channel, alertPayload));
+						System.err.println(String.format("sending this; channel=%s, payload=%s", channel, alertPayload));
 						try{
-						  String r=Http.post(channel, alertPayload, new MapBuilder<String, String>().put("Content-Type", "application/json; charset=UTF-8").build());
-						  System.out.println("Sending '"+alertPayload+"' to google chat api. response.code=200");
+							String r=Http.post(channel, alertPayload, new MapBuilder<String, String>().put("Content-Type", "application/json; charset=UTF-8").build());
+							System.out.println("Sent '"+alertPayload+"' to chat api. response.code=200");
 						}catch(HttpException e){
-						  System.out.println("Sending '"+alertPayload+"' to google chat api but hit an error: response.code="+e.getStatusCode());
+							System.out.println("Sent '"+alertPayload+"' to chat api but hit an error: response.code="+e.getStatusCode());
 						}catch(IOException e){
-						  System.out.println("Sending '"+alertPayload+"' to google chat api but hit an error: "+ e.getMessage());
+							System.out.println("Sent '"+alertPayload+"' to chat api but hit an error: "+ e.getMessage());
 						}
 					}
-//				}
+				}
 			}
 		}
 	}
 	
 	private String getTemplate(Config c, String channel){
-	  if (channel.contains("slack")) return c.getOptions().get("googlechat.webhook.template");
-	  if (channel.contains("google") && channel.contains("chat")) return c.getOptions().get("googlechat.webhook.template");
-	  return null;
+	  if (channel.contains("slack")) return c.getOptions().get("slack.webhook.template");
+		if (channel.contains("google")/* && channel.contains("chat") */) return c.getOptions().get("googlechat.webhook.template");
+	  return c.getOptions().get("other.webhook.template");
 	}
 }
